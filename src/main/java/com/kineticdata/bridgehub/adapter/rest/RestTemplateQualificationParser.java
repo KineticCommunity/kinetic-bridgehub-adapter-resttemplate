2b3f0b2f6ddb3e0aadc9bcf21d20d45e21653139
Scott Gerike <gerisc01@luther.edu>
2017-11-10 16:46:58 -0600

    Remove leftover references to service now

--- src/main/java/com/kineticdata/bridgehub/adapter/rest/RestTemplateQualificationParser.java
+++ src/main/java/com/kineticdata/bridgehub/adapter/rest/RestTemplateQualificationParser.java
@@ -1,8 +1,8 @@
-package com.kineticdata.bridgehub.adapter.servicenow;
+package com.kineticdata.bridgehub.adapter.rest;
 
 import com.kineticdata.bridgehub.adapter.QualificationParser;
 
-public class ServiceNowQualificationParser extends QualificationParser {
+public class RestTemplateQualificationParser extends QualificationParser {
     public String encodeParameter(String name, String value) {
         return value;
     }
